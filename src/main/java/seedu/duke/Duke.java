bbf47072b66f92ee61fb0da7b01b737a4ccb6075
JustinnT <e0311076@u.nus.edu>
2020-03-07 11:27:48 +0800

    Updated main to reflect changes and tasks to do.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,20 +1,21 @@
 package seedu.duke;
 
 import seedu.duke.command.Command;
+import seedu.duke.exceptions.unknownCommandException;
 import seedu.duke.parser.Parser;
 import seedu.duke.storage.Storage;
 import seedu.duke.ui.Ui;
 
-import java.util.Map;
-import java.util.Scanner;
-
 
 public class Duke {
+    public static int indexNumber;
+
     private Ui ui;
     private Parser parser;
     private Storage storage;
 
     public Duke() {
+        indexNumber = 0;
         ui = new Ui();
         parser = new Parser();
         storage = new Storage();
@@ -27,13 +28,11 @@ public class Duke {
                 String fullCommand = ui.readFromUser();
 
                 Command c = parser.parseCommand(fullCommand); //return what type of command you should execute
+                assert c != null;
+                //TODO for @andy after getting command object, what to do with it? Command object will never be null
 
-
-
-
-
-            } catch (Exception e) {
-                //TODO replace exception e with a custom exception and print a custom error message.
+            } catch (unknownCommandException e) {
+                ui.showUnknownCommandError();
             }
         }
     }
