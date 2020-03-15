adc0f31655f95297e40b88521e3b582c9f86e1da
JustinnT <e0311076@u.nus.edu>
2020-03-15 12:10:23 +0800

    created skeleton code for appointment commands

---
+++ src/main/java/seedu/duke/command/AddAppointmentCommand.java
@@ -0,0 +1,14 @@
+package seedu.duke.command;
+
+import seedu.duke.command.Command;
+import seedu.duke.storage.Storage;
+import seedu.duke.ui.Ui;
+
+import java.io.IOException;
+
+public class AddAppointmentCommand extends Command {
+
+    @Override
+    public void execute(Ui ui, Storage storage) throws IOException {
+    }
+}
---
+++ src/main/java/seedu/duke/command/ListAppointmentCommand.java
@@ -0,0 +1,15 @@
+package seedu.duke.command;
+
+import seedu.duke.command.Command;
+import seedu.duke.storage.Storage;
+import seedu.duke.ui.Ui;
+
+import java.io.IOException;
+
+public class ListAppointmentCommand extends Command {
+
+    @Override
+    public void execute(Ui ui, Storage storage) throws IOException {
+
+    }
+}
