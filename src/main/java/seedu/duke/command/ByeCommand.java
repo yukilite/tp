a4734126972ff3cb83136b38f9287c75c8016b84
JustinnT <e0311076@u.nus.edu>
2020-03-15 11:01:43 +0800

    Added skeleton code for ByeCommand

---
+++ src/main/java/seedu/duke/command/ByeCommand.java
@@ -0,0 +1,13 @@
+package seedu.duke.command;
+
+import seedu.duke.storage.Storage;
+import seedu.duke.ui.Ui;
+
+import java.io.IOException;
+
+public class ByeCommand extends Command {
+
+    @Override
+    public void execute(Ui ui, Storage storage) {
+    }
+}
