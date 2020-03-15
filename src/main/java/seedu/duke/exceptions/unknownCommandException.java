8f890d505c4195cf691d725107dc94213eed3dbc
JustinnT <e0311076@u.nus.edu>
2020-03-16 01:27:46 +0800

    fixing weird bug

--- src/main/java/seedu/duke/exceptions/unknownCommandException.java
+++
@@ -1,8 +0,0 @@
-package seedu.duke.exceptions;
-
-public class UnknownCommandException extends Exception {
-    @Override
-    public String getLocalizedMessage() {
-        return "Unknown command";
-    }
-}
