7debc8beccdb37963f4fa6568d6efca9df46aab5
Samantha Goh <>
2020-03-29 04:07:46 +0800

    Added Junit Test for timeConverter and changed the values for other junit tests to pass

---
+++ src/test/java/seedu/duke/converter/TimeConverterTest.java
@@ -0,0 +1,22 @@
+package seedu.duke.converter;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeConverterTest {
+
+    @Test
+    void testOldDate() throws ParseException {
+        String oldDate = "29/3/2020";
+        assertEquals("Sun 29 Mar 2020", TimeConverter.oldDate(oldDate));
+    }
+
+    @Test
+    void testOldTime() throws ParseException {
+        String oldTime = "2315";
+        assertEquals("11:15 pm", TimeConverter.oldTime(oldTime));
+    }
+}
