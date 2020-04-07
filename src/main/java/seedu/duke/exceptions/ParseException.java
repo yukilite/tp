f64d0b52aaa494b33dc3f0dd159a19193aaa9a8a
Samantha Goh <>
2020-04-08 01:45:38 +0800

    More edits

--- src/main/java/seedu/duke/exceptions/ParseException.java
+++ src/main/java/seedu/duke/exceptions/ParseException.java
@@ -1,12 +1,6 @@
 package seedu.duke.exceptions;
 
 class ParseException extends Exception {
-    private String command;
-
-    public ParseException(String commandType) {
-        this.command = commandType;
-    }
-
     @Override
     public String getLocalizedMessage() {
         return "Please fill in date in right format: dd/mm/yyyy and time in the 24 hour format: hhmm";
