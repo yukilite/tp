c2a363b1d22db5193bd3e8e5106bbd2baae0f1e9
brandoncjh <brandonchongjh@gmail.com>
2020-04-09 02:18:53 +0800

    another fix attempt

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -10,7 +10,7 @@ cd text-ui-test
 
 java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
-diff EXPECTEDoriginal.TXT ACTUAL.TXT
+diff EXPECTED.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
 then
     echo "Test passed!"
