bdfa8d4ab51be4e8c4e1e49419ccdfc2ae885b3f
JustinnT <e0311076@u.nus.edu>
2020-04-09 14:41:06 +0800

    Rename ParseException to InvalidDateTimeException as ParseException is the default name

--- src/main/java/seedu/duke/exceptions/InvalidDateTimeException.java
+++ src/main/java/seedu/duke/exceptions/InvalidDateTimeException.java
@@ -1,6 +1,6 @@
 package seedu.duke.exceptions;
 
-class ParseException extends Exception {
+public class InvalidDateTimeException extends Exception {
     @Override
     public String getLocalizedMessage() {
         return "Please fill in date in right format: dd/mm/yyyy and time in the 24 hour format: hhmm";
